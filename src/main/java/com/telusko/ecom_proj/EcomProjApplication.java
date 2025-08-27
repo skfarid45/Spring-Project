2871f8d3c69e11fa1ec7513cd6b9d034e8552d44
Shaik Farid <shaikfarid524@gmail.com>
2025-08-27 23:39:51 +0530

    Update EcomProjApplication.java

--- src/main/java/com/telusko/ecom_proj/EcomProjApplication.java
+++ src/main/java/com/telusko/ecom_proj/EcomProjApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcomProjApplication {
-
+public class EcomProjApplication extends SpringBootServletInitializer {
+    @Override
+	protected SpringApplicationBuilder
+	configure(SpringApplicationBuilder builder){
+		return builder.sourcea(EcomProjApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EcomProjApplication.class, args);
 	}
